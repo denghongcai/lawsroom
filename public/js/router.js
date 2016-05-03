4dc33a642581de635ce7f68fad83e96e54a2b20e
Cloud <cloud@txthinking.com>
2016-05-03 08:38:57 +0800

    bg img move to body

--- public/js/router.js
+++ public/js/router.js
@@ -1,6 +1,7 @@
 var pages = document.querySelector('#pages');
 var room = document.querySelector('#x-room');
 var tb = document.querySelector('paper-toolbar');
+var body = document.querySelector('body');
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -8,32 +9,39 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-77171491-1', 'auto');
 
-function _a(ctx){
+function _a(ctx, next){
     ga('set', 'page', ctx.path);
     ga('send', 'pageview');
 }
 
-page('/', function(ctx, next){
+function homeStyle(ctx, next){
+    body.style.backgroundImage = "url(/img/bg.jpeg)";
     tb.hidden = true;
+    next();
+}
+function clearHomeStyle(ctx, next){
+    body.style.backgroundImage = "none";
+    tb.hidden = false;
+    next();
+}
+
+page('/', function(ctx, next){
     pages.select('x-door');
     next();
-}, _a);
+}, homeStyle, _a);
 page('/random', function(ctx, next){
-    tb.hidden = false;
     pages.select('random-room');
     next();
-}, _a);
+}, clearHomeStyle, _a);
 page('/room/:id', function(ctx, next){
-    tb.hidden = false;
     room.roomId = ctx.params.id;
     pages.select('x-room');
     next();
-}, _a);
+}, clearHomeStyle, _a);
 page('*', function(ctx, next){
-    tb.hidden = true;
     pages.select('x-door');
     next();
-}, _a);
+}, homeStyle, _a);
 
 page({
     hashbang: false
